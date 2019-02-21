441c84f755c00a0bd1e71db166472e8775ecb3c9
iis-rio <e.lacerda@iis-rio.org>
2019-02-21 17:36:34 -0300

    Read of sub-region parameters started

--- plangea_functions.R
+++ plangea_functions.R
@@ -1,3 +1,17 @@
 # PLANGEA FUNCTIONS
 
 library(raster)
+library(jsonlite)
+
+calc_sparable_area = function(projected_areas){
+  # Computing overall sparable land vs demanded land
+  dem.lnd = sum(projected_areas$total[projected_areas$total>0])
+  spa.lnd = sum(projected_areas$total[projected_areas$total<=0])
+  
+  # Correcting limits to restoration to account for compensation of demanded area
+  projected_areas$total[projected_areas$total>0] = 0
+  projected_areas$total = projected_areas$total * ((dem.lnd+spa.lnd)/spa.lnd)
+  projected_areas$total = -projected_areas$total
+  
+  return(projected_areas)
+}
--- plangea_wrapper.R
+++ plangea_wrapper.R
@@ -1,7 +1,26 @@
 # WRAPPER FOR RUNNING PLANGEA BASED ON AN INPUT JSON CONFIG FILE
 
-plangea_wrapper = function(config_json_location='./plangea_config.json'){
+plangea_wrapper = function(config_json_filename='../plangea-legacy/global_config.json'){
   source('plangea_functions.R')
   
-  cfg = plangea_prepare_config(config_json_location)
+  # Read JSON config file
+  config = fromJSON(config_json_filename)
+  
+  # Define configuration objects
+  targets=as.numeric(config$scenarios$targets); names(targets) = config$scenarios$target_names
+  
+  # Prepare data structures for sub-region analyses if relevant
+  if (config$scenarios$`sub-region_scenarios`$include_subregion_scenarios){
+    sub_region_ras = raster(paste0(config$io$preprocessed_data_location_name, config$scenarios$`sub-region_scenarios`$`sub-region_folder`, config$scenarios$`sub-region_scenarios`$`sub-region_raster_name`))
+    sub_region_table = read.csv(paste0(config$io$preprocessed_data_location_name, config$scenarios$`sub-region_scenarios`$`sub-region_folder`, config$scenarios$`sub-region_scenarios`$`sub-region_names_table`))
+    if (!config$scenarios$`sub-region_scenarios`$`sub-region_flat_targets`){
+      sub_region_targets = calc_sparable_area(read.csv(paste0(config$io$preprocessed_data_location_name, config$scenarios$`sub-region_scenarios`$`sub-region_folder`, config$scenarios$`sub-region_scenarios`$`sub-region_targets`)))
+    }
+  }
+  
+
+  
+  
+  sum.anthropic = 30000000
+  targets[[which(sapply(targets, is.na))]] = sum.anthropic
 }
