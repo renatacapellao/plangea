bb38d307ed789dfe3c52ac8271c61100c87d3744
elacerda <tux.68000@gmail.com>
2019-02-27 02:08:02 -0300

    updated some random syntax

--- plangea_harmonize.R
+++ plangea_harmonize.R
@@ -1,13 +1,12 @@
 
-plangea_preprocess = function(cfg){
+plangea_preprocess = function(config){
   # set the input directory from which to load all the data for the optimisation
-  dir <- cfg$io$preprocessed_data_location_name
+  dir <- config$io$preprocessed_data_location_name
   
   # List .Rdata files saved in dir
   obj.list = dir(dir, full.names=T, pattern='.Rdata', ignore.case=T)
   
   # Loads all objects with names in obj.list
-  for (i in obj.list){load(i)}
-  
+  for (i in obj.list) { load(i) }
   
 }
